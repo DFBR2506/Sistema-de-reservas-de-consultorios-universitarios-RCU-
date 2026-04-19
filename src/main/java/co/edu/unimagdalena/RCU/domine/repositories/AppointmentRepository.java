64ba09207a41f1974c0ef3e54ff33146ff877794
Jairob4 <blancojairo58@gmail.com>
2026-04-19 13:02:53 -0500

    fixing bugs

--- src/main/java/co/edu/unimagdalena/RCU/domine/repositories/AppointmentRepository.java
+++ src/main/java/co/edu/unimagdalena/RCU/domine/repositories/AppointmentRepository.java
@@ -13,22 +13,6 @@ import org.springframework.data.repository.query.Param;
 import co.edu.unimagdalena.RCU.domine.entities.Appointment;
 import co.edu.unimagdalena.RCU.domine.entities.enums.Status;
 
-public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {
-    // aca van los query, pero tengo que implementar algunos para las validaciones del service
-    
-    boolean existsByDoctorIdAndStartAtLessThanAndEndAtGreaterThan(UUID doctorId, Instant endAt, Instant startAt); // ORM - consulta si un doctor tiene una cita que se solapa con un rango de tiempo específico
-
-    boolean existsByOfficeIdAndStartAtLessThanAndEndAtGreaterThan(UUID officeId, Instant endAt, Instant startAt); // ORM - consulta si un consultorio tiene una cita que se solapa con un rango de tiempo específico
-
-    boolean existsByPatientIdAndStartAtLessThanAndEndAtGreaterThan(UUID patientId, Instant endAt, Instant startAt); // ORM - consulta si un paciente tiene una cita que se solapa con un rango de tiempo específico
-
-    List<Appointment> findByPatientIdAndStatus(UUID patientId, Status status); // ORM - consulta las citas de un paciente por su ID y estado
-
-    List<Appointment> findByDoctorIdAndStartAtBetween(UUID doctorId, Instant startAt, Instant endAt); // ORM - consulta las citas de un doctor por su ID y un rango de tiempo específico
-
-    List<Appointment> findByOfficeIdAndStartAtBetween(UUID officeId, Instant startAt, Instant endAt); // ORM - consulta las citas de un consultorio por su ID y un rango de tiempo específico
-
-    // Para ocupación de consultorios
 public interface AppointmentRepository extends JpaRepository<Appointment, UUID> {    
     // Valida traslape de citas para un doctor (MINIMO).
     boolean existsByDoctorIdAndStartAtLessThanAndEndAtGreaterThan(UUID doctorId, Instant endAt, Instant startAt);
