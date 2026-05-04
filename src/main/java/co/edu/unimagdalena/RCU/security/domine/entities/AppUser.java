53ae8d2f3d03b9017d3f1dc2722259e6f5955455
Daniel Berdugo <dfberdugo@unimagdalena.edu.co>
2026-05-03 22:32:21 -0500

    feat: add AppUser, Role and update the springboot version 4.0.5 to 4.0.6

---
+++ src/main/java/co/edu/unimagdalena/RCU/security/domine/entities/AppUser.java
@@ -0,0 +1,36 @@
+package co.edu.unimagdalena.RCU.security.domine.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "document_number", nullable = false, unique = true, length = 50)
+    private String documentNumber;
+
+    @NotBlank
+    @Size(max = 128)
+    @Column(name = "password", nullable = false, length = 128)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role", nullable = false)
+    private Set<Role> roles; 
+}
---
+++ src/main/java/co/edu/unimagdalena/RCU/security/domine/entities/Role.java
@@ -0,0 +1,9 @@
+package co.edu.unimagdalena.RCU.security.domine.entities;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_DOCTOR,
+    ROLE_USER,
+    ROLE_RECEPTIONIST,
+    ROLE_CORDINATOR
+}
